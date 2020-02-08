0d4db1e5e7de9b85f6aa858ad64cc705192f5960
angristan <angristan@pm.me>
2020-02-09 00:36:04 +0100

    S3: Add support for region, storage class and non-AWS providers
    Following https://github.com/feedbin/feedbin/pull/396, so that feedbin is fully provider agnostic!

--- lib/s3_pool.rb
+++ lib/s3_pool.rb
@@ -3,6 +3,10 @@ S3_POOL = ConnectionPool.new(size: 10, timeout: 5) do
     provider: "AWS",
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    persistent: true
+    region: ENV["AWS_S3_REGION"] || "us-east-1",
+    host: ENV["AWS_S3_HOST"] || "s3.amazonaws.com",
+    endpoint: ENV["AWS_S3_ENDPOINT"] || nil,
+    path_style: ENV["AWS_S3_PATH_STYLE"] || false,
+    persistent: true,
   )
 end
