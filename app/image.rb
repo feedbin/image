93ef27f849cf2fb0acbc756adc6fc167deedc8a3
Ben Ubois <ben@benubois.com>
2021-03-05 17:43:29 -0800

    Misc updates.

--- app/image.rb
+++ app/image.rb
@@ -27,65 +27,56 @@ class Image
   end
 
   def smart_crop!
-    resized_width, resized_height = resized_dimensions
-
-    processed_image = ImageProcessing::Vips
+    pipeline = ImageProcessing::Vips
       .source(@file)
-      .resize_to_fill(resized_width, resized_height)
+      .resize_to_fill(resized.width, resized.height)
       .convert("jpg")
       .saver(interlace: true, strip: true, quality: 80)
 
-    temporary_image = processed_image.call
-
-    y = 0
-    x = 0
-    smart_crop = false
-
-    if resized_width == @target_width && resized_height == @target_height
-      image = temporary_image
-    elsif resized_width > @target_width
-      if center = attention_center("x", temporary_image)
-        x = center - @target_width / 2
-        x = 0 if x < 0
-      else
-        smart_crop = true
-      end
-    else
-      if center = attention_center("y", temporary_image)
-        y = center - @target_height / 2
-        y = 0 if y < 0
-      else
-        smart_crop = true
-      end
+    if resized.width == @target_width && resized.height == @target_height
+      pipeline.call(destination: persisted_path)
+      return persisted_path
     end
 
-    if smart_crop
-      image = processed_image.resize_to_fill(@target_width, @target_height, crop: :attention)
+    if resized.width > @target_width
+      axis = "x"
+      contraint = @target_width
     else
-      image = processed_image.crop(x.to_i, y.to_i, @target_width, @target_height)
+      axis = "y"
+      contraint = @target_height
+    end
+
+    if center = average_face_position(axis, pipeline.call)
+      point = {"x" => 0, "y" => 0}
+      point[axis] = center - contraint / 2
+      point[axis] = 0 if point[axis] < 0
+      image = pipeline.crop(point["x"].to_i, point["y"].to_i, @target_width, @target_height)
+    else
+      image = pipeline.resize_to_fill(@target_width, @target_height, crop: :attention)
     end
 
     image.call(destination: persisted_path)
     persisted_path
   end
 
-  def resized_dimensions
-    resized_width = @target_width.to_f
+  def resized
+    @resized ||= begin
+      resized_width = @target_width.to_f
 
-    width_proportion = width.to_f / height.to_f
-    height_proportion = height.to_f / width.to_f
+      width_proportion = width.to_f / height.to_f
+      height_proportion = height.to_f / width.to_f
 
-    resized_height = resized_width * height_proportion
+      resized_height = resized_width * height_proportion
 
-    if resized_height < @target_height
-      resized_height = @target_height.to_f
-      resized_width = resized_height * width_proportion
+      if resized_height < @target_height
+        resized_height = @target_height.to_f
+        resized_width = resized_height * width_proportion
+      end
+      OpenStruct.new({width: resized_width.to_i, height: resized_height.to_i})
     end
-
-    [resized_width.to_i, resized_height.to_i]
   end
 
-  def attention_center(dimension, file)
+  def average_face_position(axis, file)
     params = {
       pigo: Shellwords.escape(PIGO),
       image: Shellwords.escape(file.path),
@@ -103,10 +94,10 @@ class Image
     return nil if faces.nil?
 
     result = faces.flat_map {|face| face.dig("face")}.map do |face|
-      face[dimension] + face["size"] / 2
+      face[axis] + face["size"] / 2
     end
 
-    result.sum(0.0) / result.size
+    (result.sum(0.0) / result.size).to_i
   end
 
   def persisted_path
--- test/image_test.rb
+++ test/image_test.rb
@@ -6,14 +6,15 @@ class ImageTest < Minitest::Test
     image = Image.new(file, target_width: 542, target_height: 304)
     assert_equal(image.width, 640)
     assert_equal(image.height, 828)
-    assert_equal([542, 701], image.resized_dimensions)
+    assert_equal(542, image.resized.width)
+    assert_equal(701, image.resized.height)
   end
 
   def test_should_get_face_location
     file = File.expand_path("support/www/image.jpeg", __dir__)
     image = Image.new(file, target_width: 542, target_height: 304)
 
-    assert_equal(455.6, image.attention_center("y", File.new(file)))
+    assert_equal(455, image.average_face_position("y", File.new(file)))
   end
 
   def test_should_crop
@@ -21,8 +22,13 @@ class ImageTest < Minitest::Test
     image = Image.new(file, target_width: 542, target_height: 304)
     cropped_path = image.smart_crop!
     assert cropped_path.include?(".jpg")
-
-    pp Digest::SHA1.hexdigest(File.read(cropped_path))
     FileUtils.rm cropped_path
+  end
+
+  def test_should_return_same_size_image
+    file = File.expand_path("support/www/image.jpeg", __dir__)
+    image = Image.new(file, target_width: 640, target_height: 828)
+    cropped_path = image.smart_crop!
+    assert cropped_path.include?(".jpg")
   end
 end
